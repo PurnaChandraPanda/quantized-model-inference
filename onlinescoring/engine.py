44941181be4f341f218477c6c6a22b7518747997
Purna Chandra Panda <pupanda@outlook.com>
2025-01-08 02:28:09 +0000

    cuda support added for llama-cpp-python use

--- onlinescoring/engine.py
+++ onlinescoring/engine.py
@@ -16,7 +16,8 @@ class LlamacppEngine():
     def __init__(self, model_path: str):
         self._model_path = model_path
         self._client = LllamcppClient(local_api_url=f"http://{WebServer.HOST}:{WebServer.PORT}")
-
+        self._is_cuda_visible: bool = False
+    
     def load_model(self, env: Dict = None):
         """Load the model from the pretrained model specified in the engine configuration."""
         if env is None:
@@ -27,8 +28,16 @@ class LlamacppEngine():
         """
         definition to start the llama-cpp server
         """
-        cmd = ["python", "-m", "llama_cpp.server", "--model", self._model_path]
+        # Check in env if cuda is visible. 
+        ## If yes, load model on GPUs for inferencing. Else, load model CPUs for inferencing.
+        if 'NVIDIA_VISIBLE_DEVICES' in env:
+            cmd = ["python", "-m", "llama_cpp.server", "--model", self._model_path, "--n_gpu_layers", "-1"]
+            # Set the flag to True as its running on GPU
+            self._is_cuda_visible = True
+        else:
+            cmd = ["python", "-m", "llama_cpp.server", "--model", self._model_path]        
         print(f"Starting llama-cpp server with command: {cmd}")
+
         subprocess.Popen(cmd, env=env)
         self._wait_until_server_healthy(host=WebServer.HOST, port=WebServer.PORT)
         print("Starting llama-cpp server...")
@@ -62,8 +71,26 @@ class LlamacppEngine():
         """
         # print("in engine run() :: ", payload)
 
+        # # Print the CUDA usage
+        # self._print_cuda_usage()
+
         # Perform prompt inferencing on the llm model
         inference_results = self._client.generate(payload.query, payload.params, payload.task_type)
 
+        # # Print the CUDA usage
+        # self._print_cuda_usage()
+        
         return inference_results
 
+    def _print_cuda_usage(self):
+        """
+        Print the CUDA memory usage.
+        """
+        if self._is_cuda_visible:
+            try:
+                cmd = ["nvidia-smi"]
+                subprocess.run(cmd)
+            except Exception as e:
+                print(f"Failed to print CUDA usage: {e}")
+        else:
+            print("CUDA is not visible. No need to print CUDA usage.")
